a9fa4b4387754bc9e7eacfe6ec3d8529015b340d
vanec06 <chavezgalindezvanesa@gmail.com>
2022-11-17 11:44:11 -0500

    modificacion

---
+++ js/media-query.js
@@ -0,0 +1,10 @@
+let barmenu = document.getElementById("barmenu")
+let main-lu = document.getElementById("main-lu")
+
+barmenu.addEventListener("click", function() {
+   if (main-lu.classlist.contains(main-lu)) {
+
+    };
+
+    
+    })
