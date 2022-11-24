68565eb3c36a07205b794d47be6b663c3ed8a846
vanec06 <chavezgalindezvanesa@gmail.com>
2022-11-24 08:39:31 -0500

    modificacion

--- js/media-query.js
+++ js/media-query.js
@@ -1,10 +1,14 @@
-let barmenu = document.getElementById("barmenu")
-let main-lu = document.getElementById("main-lu")
+let barMenu = document.getElementById("barMenu");
+let main_Menu = document.getElementById("mainMenu");
 
-barmenu.addEventListener("click", function() {
-   if (main-lu.classlist.contains(main-lu)) {
-
-    };
-
+barMenu.addEventListener("click", function() {
+   if (main_Menu.classList.contains("main-menu")) {
+      main_Menu.classList.add("main-menu-visible");
+      main_Menu.classList.remove("main-menu");
+    }
+   else{
+      main_Menu.classList.remove("main-menu-visible");
+      main_Menu.classList.add("main-menu");
+     }
     
-    })
+    });
