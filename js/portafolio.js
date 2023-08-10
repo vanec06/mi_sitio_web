379baecbd9914d4932f8ea78e426e0afdffce17f
SENA <dajozavargas@gmail.com>
2023-08-10 11:39:20 -0500

    modificacion

---
+++ js/portafolio.js
@@ -0,0 +1,20 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const proyectos = document.querySelectorAll(".proyecto");
+
+//     proyectos.forEach((proyecto) => {
+//         proyecto.addEventListener("click", () => {
+//             mostrarContenidoProyecto(proyecto.id);
+//         });
+//     });
+
+//     function mostrarContenidoProyecto(id) {
+//         // Oculta todos los proyectos
+//         proyectos.forEach((proyecto) => {
+//             proyecto.style.display = "none";
+//         });
+
+//         // Muestra el contenido del proyecto seleccionado
+//         const proyectoSeleccionado = document.getElementById(id);
+//         proyectoSeleccionado.style.display = "block";
+//     }
+// });
