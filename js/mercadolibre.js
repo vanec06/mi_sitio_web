d854d06299fd1fd7fcdf788de94c07df831260b4
vanec06 <vanesacha06@gmail.com>
2023-10-12 07:42:30 -0500

    modificacion

--- js/mercadolibre.js
+++ js/mercadolibre.js
@@ -9,7 +9,9 @@ class Automovil{
     }
 }
 
+
 let auto1 = new Automovil("Toyota","TXL",15000,2021,"pitalito - Huila",35420);
+let auto2 = new Automovil("Toyota","TXL",15000,2021,"pitalito - Huila",35420);
 // let marcaautomovil = auto1.marca;
 let inputbusqueda = document.getElementById("inputBusqueda");
 
@@ -45,7 +47,7 @@ iconHearth.setAttribute("class","icon-style-hearth fa-regular fa-heart")
     let imgAuto = document.createElement("img");
     boxImg.appendChild(imgAuto);
     boxImg.setAttribute("class", "box-img");
-    imgAuto.setAttribute("src", "imagenes/carro2mercado.png");
+    imgAuto.setAttribute("src", "imagenes/carro3.jpg");
     imgAuto.setAttribute("class","img-auto");
 
 
@@ -103,6 +105,82 @@ mainContent.appendChild(boxlineaDiv);
 boxlineaDiv.setAttribute("class", "box-linea-div");
 boxlineaDiv.appendChild(lineaDiv);
 
-    });
+});
+
+
+// Crear un contenedor para el segundo vehículo
+let boxAuto2 = document.createElement("div");
+mainContent.appendChild(boxAuto2);
+boxAuto2.setAttribute("class", "box-auto");
+
+// Crear contenedor para la imagen
+let boxImg2 = document.createElement("div");
+
+// Corazón imagen
+let boxCorazon2 = document.createElement("div");
+boxImg2.appendChild(boxCorazon2);
+boxCorazon2.setAttribute("class", "box-corazon");
+let corazon2 = document.createElement("div");
+boxCorazon2.appendChild(corazon2);
+corazon2.setAttribute("class", "corazon");
+
+// Cargando icono corazón
+let iconHearth2 = document.createElement("i");
+corazon2.appendChild(iconHearth2);
+iconHearth2.setAttribute("class", "icon-style-hearth fa-regular fa-heart");
+
+boxAuto2.appendChild(boxImg2);
+let imgAuto2 = document.createElement("img");
+boxImg2.appendChild(imgAuto2);
+boxImg2.setAttribute("class", "box-img");
+imgAuto2.setAttribute("src", "imagenes/carro1.jpg");
+imgAuto2.setAttribute("class", "img-auto");
+
+// Crear contenedor para la información
+let boxInfo2 = document.createElement("div");
+boxAuto2.appendChild(boxInfo2);
+let infoMarca2 = document.createElement("div");
+boxInfo2.appendChild(infoMarca2);
+boxInfo2.setAttribute("class", "box-info");
+infoMarca2.setAttribute("class", "info-marca");
+let txtMarca2 = document.createElement("label");
+infoMarca2.appendChild(txtMarca2);
+txtMarca2.innerText = auto2.marca;
+txtMarca2.setAttribute("class", "txt-marca");
+
+let txtModelo2 = document.createElement("label");
+infoMarca2.appendChild(txtModelo2);
+txtModelo2.innerText = " " + auto2.modelo;
+txtModelo2.setAttribute("class", "txt-modelo");
+
+// Precio
+let precioFormat2 = new Intl.NumberFormat("de-DE").format(auto2.precio);
+let txtPrecio2 = document.createElement("label");
+boxInfo2.appendChild(txtPrecio2);
+txtPrecio2.innerText = "$" + precioFormat2;
+txtPrecio2.setAttribute("class", "txt-precio");
+
+// Procedencia y kilometraje
+let boxProcedencia2 = document.createElement("div");
+boxInfo2.appendChild(boxProcedencia2);
+boxProcedencia2.setAttribute("class", "box-procedencia");
+
+let txtAnyo2 = document.createElement("label");
+boxProcedencia2.appendChild(txtAnyo2);
+txtAnyo2.innerHTML = auto2.anyo + " · ";
+
+let txtKilometros2 = document.createElement("label");
+boxProcedencia2.appendChild(txtKilometros2);
+let txtKilometrosFormat2 = new Intl.NumberFormat("de-DE").format(auto2.kilometros);
+txtKilometros2.innerHTML = txtKilometrosFormat2 + " · ";
+
+let txtCiudad2 = document.createElement("label");
+boxProcedencia2.appendChild(txtCiudad2);
+txtCiudad2.innerHTML = auto2.ciudad;
+
+let boxlineaDiv2 = document.createElement("div");
+mainContent.appendChild(boxlineaDiv2);
+boxlineaDiv2.setAttribute("class", "box-linea-div");
+boxlineaDiv2.appendChild(lineaDiv);
 
 
