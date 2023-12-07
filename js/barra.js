f94e4fff4990b99265a87eb8bf01cca58dca9ab8
vanec06 <vanesacha06@gmail.com>
2023-12-07 08:11:53 -0500

    mofifi

--- js/barra.js
+++ js/barra.js
@@ -1,28 +1,22 @@
-function actualizarBarras() {
-    // Obtener la puntuación de cada barra desde los input
-    var puntuaciones = [];
-    var inputs = document.getElementsByClassName('cuadro');
-    
-    for (var i = 0; i < inputs.length; i++) {
-        puntuaciones.push(inputs[i].value);
-    }
-  
-    // Actualizar el estilo de cada barra y el número según la puntuación
-    for (var i = 0; i < puntuaciones.length; i++) {
-        var barra = document.getElementsByClassName('barra')[i];
-        var numero = barra.querySelector('.numero');
-  
-        barra.style.height = puntuaciones[i] + 'px';
-        barra.classList.add('lleno');
-        numero.textContent = puntuaciones[i];
-    }
+function llenarBarras() {
+    let estrella5 = parseFloat(document.getElementById("estrella5").value) || 0;
+    let estrella4 = parseFloat(document.getElementById("estrella4").value) || 0;
+    let estrella3 = parseFloat(document.getElementById("estrella3").value) || 0;
+    let estrella2 = parseFloat(document.getElementById("estrella2").value) || 0;
+    let estrella1 = parseFloat(document.getElementById("estrella1").value) || 0;
 
-    // Vaciar los inputs y establecerlos en cero
-    for (var i = 0; i < inputs.length; i++) {
-        inputs[i].value = '' ;
+    let sumaTotal = estrella5 + estrella4 + estrella3 + estrella2 + estrella1;
+
+    if (sumaTotal > 0) {
+      let barras = document.querySelectorAll("#starBars progress");
+      let labels = document.querySelectorAll("#starBars .progreso");
+
+      barras.forEach((bar, index) => {
+        let porcentaje = (eval(`estrella${5 - index}`) / sumaTotal) * 100; 
+        bar.value = porcentaje;
+        labels[index].textContent = `${porcentaje.toFixed()}%`;
+      });
     }
-}
+  }
 
-  
 
-  
